cf42579ce0d469fed4aab22f870b65e09223dd39
Lovi <1442643087@qq.com>
2024-09-18 17:22:55 +0800

    💥 feat(compiler): 实现会员中心页面并整合至导航

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,9 @@ import CartList from "@/views/CartList/index.vue"
 import Checkout from "@/views/Checkout/index.vue"
 import Pay from '@/views/Pay/index.vue'
 import PayBack from "@/views/Pay/PayBack.vue";
+import UserInfo from "@/views/Member/components/UserInfo.vue";
+import UserOrder from "@/views/Member/components/UserOrder.vue";
+import Member from '@/views/Member/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -56,6 +59,20 @@ const router = createRouter({
       path: '/paycallback', // 注意路径，必须是paycallback
       component: PayBack
     },
+    {
+      path: '/member',
+      component: Member,
+      children: [
+        {
+          path: '',
+          component: UserInfo
+        },
+        {
+          path: '/order',
+          component: UserOrder
+        }
+      ]
+    }
   ],
   //定制路由滚动行为,进入新页面的时候
   scrollBehavior(){
